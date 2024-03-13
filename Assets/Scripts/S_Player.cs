884e53c2b1576179fddb498ca26f1daa6e9d4ea1
Undergamer64 <baptiste.dufosse2005@gmail.com>
2024-03-13 21:37:18 +0100

    fixed movement and physics

--- Assets/Scripts/S_Player.cs
+++ Assets/Scripts/S_Player.cs
@@ -12,41 +12,51 @@ public class S_Player : MonoBehaviour
     [SerializeField]
     private float m_jumpPower;
 
+    [SerializeField]
+    private float m_speed = 1f;
+
     private bool m_isShooting;
 
     [SerializeField]
-    private float m_shootCooldown = 0.25f;
+    private float m_maxShootCooldown = 0.15f;
+    private float m_shootCooldown;
 
     private void Awake()
     {
         m_rigidBody = GetComponent<Rigidbody2D>();
         m_NbShoot = m_MaxNbShoot;
+        m_shootCooldown = m_maxShootCooldown;
     }
 
     private void Update()
     {
+        Shoot();
+    }
+
+    private void Shoot()
+    {
         if (m_shootCooldown <= 0)
         {
-            Shoot();
-            m_shootCooldown = 0.25f;
+            if (m_isShooting && m_NbShoot > 0)
+            {
+                if (m_rigidBody.velocity.y <= -0.5f)
+                {
+                    m_rigidBody.velocity = new Vector2(m_rigidBody.velocity.x, -0.5f);
+                }
+                m_rigidBody.velocity += Vector2.up * 0.25f;
+                m_NbShoot--;
+                m_shootCooldown = 0.25f;
+            }
+        }
+        else if (m_IsGrounded)
+        {
+            m_shootCooldown = m_maxShootCooldown;
         }
         else
         {
             m_shootCooldown -= Time.deltaTime;
         }
-    }
-
-    private void Shoot()
-    {
-        if (m_isShooting && m_NbShoot > 0)
-        {
-            if (m_rigidBody.velocity.y <= -0.5f)
-            {
-                m_rigidBody.velocity = new Vector2(m_rigidBody.velocity.x, -0.5f);
-            }
-            m_rigidBody.velocity += Vector2.up * 0.25f;
-            m_NbShoot--;
-        }
+        
     }
 
     public void JumpOrShoot(InputAction.CallbackContext context)
@@ -55,7 +65,7 @@ public class S_Player : MonoBehaviour
         {
             if (m_IsGrounded)
             {
-                m_rigidBody.velocity = Vector2.up * m_jumpPower;
+                m_rigidBody.velocity = new Vector2(m_rigidBody.velocity.x, m_jumpPower);
                 m_IsGrounded = false;
                 m_isShooting = false;
             }
@@ -66,7 +76,10 @@ public class S_Player : MonoBehaviour
         }
         else if (context.canceled) 
         {
-            m_rigidBody.velocity = new Vector2(m_rigidBody.velocity.x, m_rigidBody.velocity.y / 1.5f);
+            if (m_rigidBody.velocity.y > 0)
+            {
+                m_rigidBody.velocity = new Vector2(m_rigidBody.velocity.x, m_rigidBody.velocity.y / 1.5f);
+            }
             m_isShooting = false;
             m_shootCooldown = 0;
         }
@@ -74,13 +87,6 @@ public class S_Player : MonoBehaviour
 
     public void Move(InputAction.CallbackContext context)
     {
-        if (context.started)
-        {
-
-        }
-        if (context.canceled)
-        {
-
-        }
+        m_rigidBody.velocity = new Vector2(context.ReadValue<float>() * m_speed, m_rigidBody.velocity.y);
     }
 }
